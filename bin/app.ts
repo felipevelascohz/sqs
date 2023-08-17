148760b1b1386f02571bc5ec48644430ccede1f2
Felipe Velasco <felipe.velasco@baaskit.com>
2023-08-17 15:28:11 -0400

    se agrega DLQ

--- bin/app.ts
+++ bin/app.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Sqs } from '../src/sqs';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { Aspects } from 'aws-cdk-lib';
 
 const iniciativa :string = 'IdSbxFelipeVelasco';
 const provider = {
@@ -10,11 +12,17 @@ const provider = {
 };
 
 const app = new cdk.App();
-
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 const stack = new cdk.Stack(app, iniciativa, {
   env: provider
-})
+});
 
-new Sqs(stack, iniciativa, {})
+const DLQ = new Sqs(stack, iniciativa + 'DLQ', {})
+new Sqs(stack, iniciativa, {}, {
+  deadLetterQueue: {
+    queue: DLQ,
+    maxReceiveCount: 10
+  }
+});
 
 app.synth();
